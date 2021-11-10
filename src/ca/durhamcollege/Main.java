b81ea528a1d79a2f62866180508a9ff3bff14b85
Arsalan Radhu <arsalan.radhu@dcmail.ca>
2021-11-10 14:58:11 -0500

    Final Changes

--- src/ca/durhamcollege/Main.java
+++ src/ca/durhamcollege/Main.java
@@ -13,28 +13,26 @@ public class Main
 
     public static void main(String[] args)
     {
-        //Constants
-        final int GAMES =   2;
-        final int PLAYERS = 3;
+        //CONSTANTS
+        final int NUMBER_OF_GAMES =   2;
+        final int NUMBER_OF_PLAYERS = 3;
         final int MIN_SCORE = 0;
         final int MAX_SCORE = 300;
 
         //Declarations
         String[] playerNames = {"Arsalan", "Affan", "Tom"};
-        Scanner keyboard = new Scanner(System.in);
         boolean valid = true;
         int tempScore = 0;
         double playerAverageScore = 0;
+        Scanner keyboard = new Scanner(System.in);
         //declaring a 2D Array
-        int[][] playerGames = new int [PLAYERS][GAMES];
+        int[][] playerGames = new int [NUMBER_OF_PLAYERS][NUMBER_OF_GAMES];
 
 
         //Nested for loops for taking user input
-        for (int playerCounter = 0; playerCounter < PLAYERS; playerCounter++) //Player counter
+        for (int playerCounter = 0; playerCounter < NUMBER_OF_PLAYERS; playerCounter++) //Player counter
         {
-            //System.out.printf("Please enter %s's score for ", playerNames[playerCounter]);
-
-            for (int gameCounter = 0; gameCounter < GAMES; gameCounter++) //Game counters
+            for (int gameCounter = 0; gameCounter < NUMBER_OF_GAMES; gameCounter++) //Game counters
             {
                 do {
                     //While the input is invalid, keep asking for game scores
@@ -44,14 +42,15 @@ public class Main
                         System.out.printf("GAME #%d: ", gameCounter + 1);
                         tempScore = keyboard.nextInt();
 
-                        //if the user input is higher or lower than the acceptable inputs
+                        //if the user input is greater than or less than MAX_SCORE and MIN_SCORE
                         if (tempScore < MIN_SCORE || tempScore > MAX_SCORE)
                         {
                             System.out.print("\nYou must enter a valid score between"+MIN_SCORE +" and " +MAX_SCORE+ "\n");
                             keyboard.nextLine();
                             valid = true;
                         }
-                        else //if the input is valid
+                        //if the valid input is entered
+                        else
                         {
                             valid = false;
                             playerGames[playerCounter][gameCounter] = tempScore;
@@ -64,25 +63,24 @@ public class Main
                         keyboard.nextLine();
                         valid = true;
                     }
-                } while (valid); //using a boolean
+                } while (valid);
             }
         }
 
-        //PRINT THE ARRAY
-        for (int playerCounter = 0; playerCounter < PLAYERS; playerCounter++)
+        //Printing the output (array)
+        for (int playerCounter = 0; playerCounter < NUMBER_OF_PLAYERS; playerCounter++)
         {
-
-            System.out.printf("Score details for %s: \n", playerNames[playerCounter]);
+            System.out.printf("\nScore details for %s: \n", playerNames[playerCounter]);
             playerAverageScore = 0;
 
-            for (int GameCounter = 0; GameCounter < GAMES; GameCounter++)
+            for (int GameCounter = 0; GameCounter < NUMBER_OF_GAMES; GameCounter++)
             {
                 System.out.printf("Game # %d: ", GameCounter + 1);
                 System.out.println(playerGames[playerCounter][GameCounter]);
                 //take the number (as a string) and cast it as an integer, then store it in playerAvg
                 playerAverageScore += playerGames[playerCounter][GameCounter];
             }
-            System.out.printf("Average : %.1f\n", (playerAverageScore / GAMES));
+            System.out.printf("Average : %.1f\n", (playerAverageScore / NUMBER_OF_GAMES));
         }
 
     }
